28bc4ec059215c7b0246aeaac6c967b92ecc382a
linn063 <160041774+linn063@users.noreply.github.com>
2024-02-14 10:35:55 -0500

    First Python Upload

---
+++ P1LAB1_LinneyJeffrey.py
@@ -0,0 +1,10 @@
+#Jeffrey Linney
+#2/12/2024
+#P1LAB1
+#Using input and output
+
+#Creating a variable to hold first name
+first_name = input("Enter your first name: ")
+
+#Print first_name with greeting
+print("Hey",first_name, "Welcome to zyBooks")
